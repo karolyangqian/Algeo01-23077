1391eebacb683d41209143f70f960859a8df36a7
karolyangqian <karolyangqian14@gmail.com>
2024-10-02 23:58:20 +0700

    feat: add SistemPersamaanLininer class definition file

---
+++ src/SistemPersamaanLinier.java
@@ -0,0 +1,13 @@
+public class SistemPersamaanLinier {
+    /**
+     * Konstruktor kelas SistemPersamaanLinier
+     */
+    public SistemPersamaanLinier(){}
+
+    /**
+     * @param M matriks augmented sistem persamaan linier
+     * @return matriks solusi dari sistem persamaan linier
+     */
+    Matriks metodeGauss(Matriks M) {
+    }
+}
