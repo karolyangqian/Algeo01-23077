192e89e98389066167ebc0cd7c21ec699fb4fc7d
Grace Evelyn Simon <19623035@mahasiswa.itb.ac.id>
2024-10-17 13:33:10 +0700

    fix: gauss elimination method

--- src/gui/src/main/java/algeo/LinearRegression.java
+++ src/gui/src/main/java/algeo/LinearRegression.java
@@ -2,6 +2,7 @@ package algeo;
 
 public class LinearRegression {
     /* untuk menambahkan quadratic features dalam metode Quadratic Linear Regression */
+    /* sebelum diolah dengan Normal Equation */
     public Matriks addQuadratic(Matriks X) {
         int row = X.getRow();
         int col = X.getCol();
@@ -28,9 +29,11 @@ public class LinearRegression {
         }
         return newX;
     }
-    /* perhitungan Miltiple Linear Regression dengan Normal Equation */
+
+    /* perhitungan Multiple Linear Regression dengan Normal Equation */
     public Matriks normalEquation(Matriks X, Matriks Y) {
         Linalg linalg = new Linalg();
+        SistemPersamaanLinier SPL = new SistemPersamaanLinier();
         int row = X.getRow();
         int col = X.getCol();
         Matriks X_bias = new Matriks(row, col + 1);
@@ -41,17 +44,21 @@ public class LinearRegression {
                 X_bias.Mat[i][j + 1] = X.Mat[i][j];
             }
         }
+
         // b = (X^T * X)^-1 * X^T * Y
+        // (X^T * X) b = X^T * Y
+        // metode Eliminasi Gauss untuk mendapatkan b dengan membentuk matriks augmented [XTX | XTY]
+
         // X^T
         Matriks XT = linalg.transposeMatriks(X_bias);
         // X^T * X
         Matriks XTX = linalg.perkalianMatriks(XT, X_bias);
-        // XTX inverse
-        Matriks XTX_inv = linalg.inversMatriks(XTX);
-        // XTX inverse * X^T
-        Matriks XTX_inv_XT = linalg.perkalianMatriks(XTX_inv, XT);
-        // XTX inverse * X^T * Y
-        Matriks b = linalg.perkalianMatriks(XTX_inv_XT, Y);
+        // X^T * Y
+        Matriks XTY = linalg.perkalianMatriks(XT, Y);
+        // matriks augmented [XTX | XTY]
+        Matriks augmented = XTX.concat(XTY, true);
+        // matriks b
+        Matriks b = SPL.metodeGauss(augmented);
 
         return b;
     }
