72a89b751af5ee4dc1d993cae17bcb8bcfec0cc4
karolyangqian <karolyangqian14@gmail.com>
2024-10-24 05:17:13 +0700

    feat: image resizer gui

--- src/gui/src/main/java/module-info.java
+++ src/gui/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires javafx.swing;
 
     opens com.myapp to javafx.fxml;
     exports com.myapp;
